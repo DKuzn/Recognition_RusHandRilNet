4b4a26e9f4d41450b4072fa2f65aee359ebe532c
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2020-10-27 16:09:11 +0300

    add torch model class

---
+++ src/torch_implementation/R2HandRilNet.py
@@ -0,0 +1,58 @@
+import torch
+
+
+class R2HandRilNet(torch.nn.Module):
+    def __init__(self):
+        super(R2HandRilNet, self).__init__()
+
+        self.conv1 = torch.nn.Conv2d(
+            in_channels=1, out_channels=124, kernel_size=3, padding=0)
+        self.act1 = torch.nn.ReLU()
+        self.conv2 = torch.nn.Conv2d(
+            in_channels=124, out_channels=124, kernel_size=3, padding=0)
+        self.act2 = torch.nn.ReLU()
+        self.pool1 = torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+
+        self.conv3 = torch.nn.Conv2d(
+            in_channels=124, out_channels=596, kernel_size=3, padding=0)
+        self.act3 = torch.nn.ReLU()
+        self.conv4 = torch.nn.Conv2d(
+            in_channels=596, out_channels=596, kernel_size=3, padding=0)
+        self.act4 = torch.nn.ReLU()
+        self.pool2 = torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+
+        self.conv5 = torch.nn.Conv2d(
+            in_channels=596, out_channels=1192, kernel_size=3, padding=0)
+        self.act5 = torch.nn.ReLU()
+        self.pool3 = torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+
+        self.fc1 = torch.nn.Linear(1192, 1192)
+        self.act7 = torch.nn.ReLU()
+
+        self.fc2 = torch.nn.Linear(1192, 62)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.act1(x)
+        x = self.conv2(x)
+        x = self.act2(x)
+        x = self.pool1(x)
+
+        x = self.conv3(x)
+        x = self.act3(x)
+        x = self.conv4(x)
+        x = self.act4(x)
+        x = self.pool2(x)
+
+        x = self.conv5(x)
+        x = self.act5(x)
+        x = self.pool3(x)
+
+        x = x.view(x.size(0), x.size(1) * x.size(2) * x.size(3))
+
+        x = self.fc1(x)
+        x = self.act7(x)
+
+        x = self.fc2(x)
+
+        return x
