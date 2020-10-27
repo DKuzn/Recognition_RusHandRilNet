f11c007f6f9d64fd4761fe539adcb6acc0aaf250
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2020-10-27 16:09:39 +0300

    add torch train function

---
+++ src/torch_implementation/train_torch.py
@@ -0,0 +1,120 @@
+import torch
+import random
+import json
+import numpy as np
+import os
+from torch_implementation.dataset_torch import R2HandRilDataset
+from torch_implementation.R2HandRilNet import R2HandRilNet
+
+random.seed(0)
+np.random.seed(0)
+torch.manual_seed(0)
+torch.cuda.manual_seed(0)
+torch.backends.cudnn.deterministic = True
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+def train(x_train, y_train, x_test, y_test, epochs=100, batch_size=100):
+    model = R2HandRilNet()
+
+    checkpoint_best = '../model/weights/best_weights.pt'
+    checkpoint_last = '../model/weights/last_weights.pt'
+    train_plot_path = '../log/train_plot.json'
+
+    model = model.to(device)
+
+    loss_function = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1.0e-3)
+
+    x_train = x_train.unsqueeze(1).float()
+    x_test = x_test.unsqueeze(1).float()
+
+    train_accuracy_history = []
+    train_loss_history = []
+    test_accuracy_history = []
+    test_loss_history = []
+
+    less_loss = 1e+10
+
+    for epoch in range(epochs):
+        train_loss = 0
+        train_accuracy = 0
+
+        order = np.random.permutation(len(x_train))
+        for start_index in range(0, len(x_train), batch_size):
+            optimizer.zero_grad()
+
+            batch_indexes = order[start_index:start_index + batch_size]
+
+            x_batch = x_train[batch_indexes].to(device)
+            y_batch = y_train[batch_indexes].to(device)
+
+            preds = model.forward(x_batch)
+            train_accuracy = (preds.argmax(dim=1) == y_batch).float().mean().data.cpu()
+
+            loss_value = loss_function(preds, y_batch)
+            train_loss = loss_value
+
+            loss_value.backward()
+
+            optimizer.step()
+
+        train_accuracy_history.append(float(train_accuracy))
+        train_loss_history.append(float(train_loss))
+
+        test_loss = 0
+        test_accuracy = 0
+
+        for start_index in range(0, len(x_test), batch_size):
+
+            x_test_batch = x_test[start_index:start_index + batch_size].to(device)
+            y_test_batch = y_test[start_index:start_index + batch_size].to(device)
+
+            test_preds = model.forward(x_test_batch)
+
+            test_accuracy = (test_preds.argmax(dim=1) == y_test_batch).float().mean().data.cpu()
+            test_loss = loss_function(test_preds, y_test_batch).data.cpu()
+
+        if test_loss < less_loss:
+            torch.save(model.state_dict(), checkpoint_best)
+            print('Test loss improve from {0} to {1}. Saving to {2}'.format(less_loss, test_loss, checkpoint_best))
+            less_loss = test_loss
+        else:
+            print('Test loss did not improve from {0}.'.format(less_loss))
+
+        test_loss_history.append(float(test_loss))
+        test_accuracy_history.append(float(test_accuracy))
+
+        print('Epoch {0}/{1} - train_loss: {2} - train_accuracy: {3} ' \
+              '- test_loss: {4} - test_accuracy: {5}'.format(epoch + 1, epochs,
+                                                             round(float(train_loss), 4),
+                                                             round(float(train_accuracy), 4),
+                                                             round(float(test_loss), 4),
+                                                             round(float(test_accuracy), 4)))
+
+    torch.save(model.state_dict(), checkpoint_last)
+
+    if os.path.exists(train_plot_path):
+        out_result = json.load(open(train_plot_path, 'r'))
+    else:
+        out_result = [[], [], [], []]
+
+    out_result[0] += train_accuracy_history
+    out_result[1] += test_accuracy_history
+    out_result[2] += train_loss_history
+    out_result[3] += test_loss_history
+    with open(train_plot_path, 'w') as f:
+        json.dump(out_result, f)
+
+
+if __name__ == '__main__':
+    train_ds = R2HandRilDataset('../R2HandRilDataset/Train')
+    test_ds = R2HandRilDataset('../R2HandRilDataset/Test')
+
+    x_train = train_ds.data()
+    y_train = train_ds.targets()
+    x_test = test_ds.data()
+    y_test = test_ds.targets()
+
+    train(x_train, y_train, x_test, y_test)
